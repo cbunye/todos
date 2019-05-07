ff3dcc46a6fc887080a36d265ec8b17a9055d7d8
Ceris <ceris.bunye@gmail.com>
2019-05-07 22:33:55 +0800

    first commit

---
+++ build/precache-manifest.82d70ba17ba3dd312bb6a1113dd22be9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70c3613e6669291f8f89",
+    "url": "/cbunye/todos/static/css/main.7c7dba21.chunk.css"
+  },
+  {
+    "revision": "70c3613e6669291f8f89",
+    "url": "/cbunye/todos/static/js/main.70c3613e.chunk.js"
+  },
+  {
+    "revision": "4ce09a3ee33534be7885",
+    "url": "/cbunye/todos/static/js/runtime~main.4ce09a3e.js"
+  },
+  {
+    "revision": "c776370a155ea97a025c",
+    "url": "/cbunye/todos/static/js/2.c776370a.chunk.js"
+  },
+  {
+    "revision": "affb34042c6d751bc28054a337ae0cac",
+    "url": "/cbunye/todos/index.html"
+  }
+];
